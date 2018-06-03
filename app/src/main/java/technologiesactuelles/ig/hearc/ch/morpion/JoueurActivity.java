d4d5f6ba89dccd59d3a4ad847bbe3d9752578810
mvoyame <mael.voyame@he-arc.ch>
2018-06-03 20:20:30 +0200

    Ajout des composants graphique

---
+++ app/src/main/java/technologiesactuelles/ig/hearc/ch/morpion/JoueurActivity.java
@@ -0,0 +1,29 @@
+package technologiesactuelles.ig.hearc.ch.morpion;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class JoueurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joueur);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
